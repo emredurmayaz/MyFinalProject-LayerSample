9d1a48a7f9439eeaa427b34b60bf96aa8cd67414
Emre Durmayaz <emredurmayaz@hotmail.com>
2021-08-26 16:15:58 +0300

    Order table Added to Project

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,15 +11,25 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            // ProductTest();
+            //IoC
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
 
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
             foreach (var product in productManager.GetAllByCategory(2))
             {
                 Console.WriteLine(product.ProductName);
             }
-
-            
         }
     }
 }
--- Entities/Concrete/Category.cs
+++ Entities/Concrete/Category.cs
@@ -8,6 +8,6 @@ namespace Entities.Concrete
     public class Category:IEntity
     {
         public int CategoryId { get; set; }
-        public string CategoryNames { get; set; }
+        public string CategoryName { get; set; }
     }
 }
