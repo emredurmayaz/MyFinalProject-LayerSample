8b700bd6a026ee9ea0a6d33e57b0bbc392369c16
Emre Durmayaz <emredurmayaz@hotmail.com>
2021-09-01 15:13:12 +0300

    Error and Success Results are added

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -18,6 +19,18 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            //business codes
+            if(product.ProductName.Length<2)
+            {
+                return new ErrorResult("Ürün ismi min 2 karakter olmalıdır!");
+            }
+            _productDal.Add(product);
+
+            return new SuccessResult("Ürün eklendi");
+        }
+
         public List<Product> GetAll()
         {
             //Job Codes
@@ -28,6 +41,11 @@ namespace Business.Concrete
         public List<Product> GetAllByCategory(int id)
         {
             return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
